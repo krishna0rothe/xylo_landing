f5dc96c3dc5a28aaa32efea4771aa517debb9e6d
Krushna <krishnarothe103@gmail.com>
2025-01-24 11:11:17 +0530

    updated routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />  
+        <Route path="/xylo_home" element={<LandingPage />} />  
       </Routes>
     </Router>
   )
